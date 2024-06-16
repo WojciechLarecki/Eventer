d5611d095ade3e5dc8f9d0ab57675327ac5a4725
Wojciech <s205838@sggw.edu.pl>
2024-06-16 22:37:56 +0200

    Add repository manager.

---
+++ Eventer/Eventer.Data/Repositories/RepositoryManager.cs
@@ -0,0 +1,26 @@
+﻿using Eventer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Repositories
+{
+    public class RepositoryManager
+    {
+        private readonly EventerContext _context;
+        private readonly Lazy<UserRepository> _userRepo;
+        private readonly Lazy<EventsRepository> _eventsRepo;
+        public RepositoryManager(EventerContext context)
+        {
+            _context = context;
+            _userRepo = new Lazy<UserRepository>(() => new UserRepository(context));
+            _eventsRepo = new Lazy<EventsRepository>(() => new EventsRepository(context));
+        }
+
+        public UserRepository UserRepository => _userRepo.Value;
+        public EventsRepository EventsRepository => _eventsRepo.Value;
+        public void Save() => _context.SaveChanges();
+    }
+}
