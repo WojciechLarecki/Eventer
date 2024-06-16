f14660fb1b777ba0b63937fe316205e5e379163a
Wojciech <s205838@sggw.edu.pl>
2024-06-16 22:25:24 +0200

    Add basic repository.

---
+++ Eventer/Eventer.Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(bool trackChanges = false, Expression<Func<T, bool>>? condition = null);
+        T? GetOne(bool trackChanges = false, Expression<Func<T, bool>>? condition = null);
+    }
+}
---
+++ Eventer/Eventer.Data/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Eventer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly EventerContext _context;
+        protected Repository(EventerContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<T> GetAll(bool trackChanges = false, Expression<Func<T, bool>>? condition = null)
+        {
+            var query = _context.Set<T>().AsQueryable();
+
+            if (!trackChanges)
+                query = query.AsNoTracking();
+            if (condition != null)
+                query = query.Where(condition);
+
+            return query.ToList();
+        }
+
+        public T? GetOne(bool trackChanges = false, Expression<Func<T, bool>>? condition = null)
+        {
+            var query = _context.Set<T>().AsQueryable();
+
+            if (!trackChanges)
+                query = query.AsNoTracking();
+            if (condition != null)
+                query = query.Where(condition);
+
+            return query.FirstOrDefault();
+        }
+    }
+}
