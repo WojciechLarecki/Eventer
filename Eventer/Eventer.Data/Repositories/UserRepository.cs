dfe4045751fb43716a8dd81fe2a166add382682d
Wojciech <s205838@sggw.edu.pl>
2024-06-16 22:25:35 +0200

    Add User reposiotry.

---
+++ Eventer/Eventer.Data/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Eventer.Data.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(EventerContext context) : base(context)
+        {
+        }
+
+        public User? Find(Guid id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
