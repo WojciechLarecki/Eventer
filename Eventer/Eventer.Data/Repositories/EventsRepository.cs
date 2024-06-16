e815de4b89cf2665b34c7dc600e3cca1c19bbb4d
Wojciech <s205838@sggw.edu.pl>
2024-06-16 22:27:39 +0200

    Add Events repository.

---
+++ Eventer/Eventer.Data/Repositories/EventsRepository.cs
@@ -0,0 +1,21 @@
+﻿using Eventer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Repositories
+{
+    public class EventsRepository : Repository<Event>
+    {
+        public EventsRepository(EventerContext context) : base(context)
+        {
+        }
+
+        public Event? Find(Guid id)
+        {
+            return _context.Events.Find(id);
+        }
+    }
+}
