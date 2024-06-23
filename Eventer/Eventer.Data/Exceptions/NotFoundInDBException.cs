7c65dd3586112f68b7965c0a33135e9540fab668
Wojciech <s205838@sggw.edu.pl>
2024-06-23 21:27:14 +0200

    Add NotFoundInDBException class.

---
+++ Eventer/Eventer.Data/Exceptions/NotFoundInDBException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Data.Exceptions
+{
+    public class NotFoundInDBException : Exception
+    {
+        public NotFoundInDBException(string? message) : base(message)
+        {
+        }
+    }
+}
