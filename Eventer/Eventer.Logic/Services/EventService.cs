515bf4e2605d43f7211674d84e1d433a3c379e49
Wojciech <s205838@sggw.edu.pl>
2024-07-30 00:27:21 +0200

    Add EventService class.

---
+++ Eventer/Eventer.Logic/Services/EventService.cs
@@ -0,0 +1,27 @@
+﻿using Eventer.Data.Repositories;
+using Eventer.Logic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Logic.Services
+{
+    public class EventService
+    {
+        private readonly IRepositoryManager _repoManager;
+        public EventService(IRepositoryManager manager)
+        {
+            _repoManager = manager;
+        }
+
+        public IEnumerable<EventDTO> GetEvents()
+        {
+            var events = _repoManager.EventsRepository.GetAll();
+            var eventDTOs = events.ToDTOs();
+
+            return eventDTOs;
+        }
+    }
+}
