ee5abffad711c131e14ce100c572804ab21c3067
Wojciech <s205838@sggw.edu.pl>
2024-08-02 19:51:30 +0200

    [E001] Add CommonValidator class.

---
+++ Eventer/Eventer.Logic/Validators/CommonValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventer.Logic.Validators
+{
+    public static class CommonValidator
+    {
+        public static void CheckIfNotNull<T>(T? date)
+        {
+            if (date == null)
+            {
+                var type = typeof(T);
+                throw new ArgumentNullException($"Object of type {type} cannot be null.");
+            }
+        }
+    }
+}
