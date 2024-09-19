66e6dee120693a4eec4f6e61ba61194392100798
Wojciech <s205838@sggw.edu.pl>
2024-09-19 23:37:47 +0200

    [E010] Set validation to EventCreateDTO.

--- Eventer/Eventer.Logic/DTOs/CreateDTOs/EventCreateDTO.cs
+++ Eventer/Eventer.Logic/DTOs/CreateDTOs/EventCreateDTO.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Eventer.Logic.DTOs.CreateDTOs
 {
-    public class EventCreateDTO
+    public class EventCreateDTO : IValidatableObject
     {
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string? Name { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? JoinDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (StartDate < DateTime.Now)
+            {
+                yield return new ValidationResult(
+                    $"Start date cannot be set earlier the presence.",
+                    new[] { nameof(StartDate) });
+            }
+            if (EndDate <= StartDate)
+            {
+                yield return new ValidationResult(
+                    $"End date cannot be set before or in the same time as start date.",
+                    new[] { nameof(EndDate) });
+            }
+        }
     }
 }
