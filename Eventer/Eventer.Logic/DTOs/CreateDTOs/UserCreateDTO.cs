933e85de3d7772d71bee4792abdea24957401bd2
Wojciech <s205838@sggw.edu.pl>
2024-09-21 15:24:06 +0200

    [E010] Add to all UserDTOs validation attributes.

--- Eventer/Eventer.Logic/DTOs/CreateDTOs/UserCreateDTO.cs
+++ Eventer/Eventer.Logic/DTOs/CreateDTOs/UserCreateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace Eventer.Logic.DTOs.CreateDTOs
 {
     public class UserCreateDTO
     {
+
+        [Required]
+        [EmailAddress] // checks if string contains '@' sign.
+        [StringLength(50)]
         public string? Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string? Password { get; set; }
+
+        [Required]
+        [Range(0, 1)]
         public byte? Role { get; set; }
     }
 }
--- Eventer/Eventer.Logic/DTOs/UserDTO.cs
+++ Eventer/Eventer.Logic/DTOs/UserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,20 @@ namespace Eventer.Logic.DTOs
 {
     public class UserDTO
     {
-
+        [Required]
         public Guid? Id { get; set; }
+
+        [Required]
+        [EmailAddress] // checks if string contains '@' sign.
+        [StringLength(50)]
         public string? Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string? Password { get; set; }
+
+        [Required]
+        [Range(0, 1)]
         public byte? Role { get; set; }
     }
 }
